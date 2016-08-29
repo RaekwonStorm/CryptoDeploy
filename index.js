1954aa71958a12cac1e44c9b12ad4bb1d4f8790a
Reed Branson <reed.branson@gmail.com>
2016-08-29 15:29:43 -0400

    established a server

---
+++ index.js
@@ -0,0 +1,8 @@
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var app = connect();
+app.use(serveStatic(__dirname + '/views'));
+app.use(serveStatic(__dirname + '/public'));
+app.listen(3333, function(){
+    console.log('Server running on 3333...');
+});
